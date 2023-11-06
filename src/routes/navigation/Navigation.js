9e6c8e3e886fb238fd22f1cc08011a46f4d972cd
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-11-06 15:47:58 -0600

    Feat: Create the sign in link in the navigation bar.

--- src/routes/navigation/Navigation.js
+++ src/routes/navigation/Navigation.js
@@ -14,6 +14,9 @@ const Navigation = () => {
           <Link className="nav-link" to="/shop">
             Shop
           </Link>
+          <Link className="nav-link" to="/signin">
+            Sign In
+          </Link>
         </div>
       </div>
       <Outlet />
