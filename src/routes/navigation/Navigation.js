083e4c83797be33831ae9becc4acfbd0b48626b3
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-11-05 21:22:29 -0600

    Feat: Update navigation bar logo.

--- src/routes/navigation/Navigation.js
+++ src/routes/navigation/Navigation.js
@@ -1,12 +1,14 @@
 import { Fragment } from 'react';
 import { Outlet, Link } from 'react-router-dom';
+import { ReactComponent as Logo } from '../../assets/store.svg';
+import './Navigation.scss';
 
 const Navigation = () => {
   return (
     <Fragment>
       <div className="navigation">
         <Link className="logo-container" to="/">
-          <div>Logo</div>
+          <Logo className="logo" />
         </Link>
         <div className="nav-links-container">
           <Link className="nav-link" to="/shop">
