46e5a566e6c6678b044089455360aa21e323eba4
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-11-06 22:10:43 -0600

    Feat: Configure Firebase instance.

---
+++ src/assets/utils/firebase/firebase.utils.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyDyndu_NzFaWL4nRsonY0ONWk7-5k8mja4',
+  authDomain: 'store-db-9d14b.firebaseapp.com',
+  projectId: 'store-db-9d14b',
+  storageBucket: 'store-db-9d14b.appspot.com',
+  messagingSenderId: '629120736789',
+  appId: '1:629120736789:web:b6ed3b2db030e90dcbb81c',
+};
+
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
