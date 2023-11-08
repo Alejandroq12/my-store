79d67fe2763604b2a0e9afb4cb5a44ed3fb13e16
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-11-07 23:08:23 -0600

    Feat: Create sign in component.

--- src/routes/sign-in/SignIn.js
+++ src/routes/sign-in/SignIn.js
@@ -1,7 +1,14 @@
+import { signInWithGooglePopup } from '../../assets/utils/firebase/firebase.utils';
 const SignIn = () => {
+  const logGoogleUser = async () => {
+    const response = await signInWithGooglePopup();
+    console.log(response);
+  };
+
   return (
     <div>
       <h1>Sign in page</h1>
+      <button onClick={logGoogleUser}>Sign in with Google popup</button>
     </div>
   );
 };
