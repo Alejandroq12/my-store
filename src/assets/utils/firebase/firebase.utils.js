1d9d3a5a5d53be85bc8867eb4df2f0f0b1389836
Alejandroq12 <julio.quezada@catolica.edu.sv>
2023-11-09 20:30:38 -0600

    Feat: Set configuration to create user from authorization.

--- src/assets/utils/firebase/firebase.utils.js
+++ src/assets/utils/firebase/firebase.utils.js
@@ -29,3 +29,13 @@ export const auth = getAuth();
 export const signInWithGooglePopup = () => signInWithPopup(auth, provider);
 
 export const db = getFirestore();
+
+export const createUserDocumentFromAuth = async (userAuth) => {
+  const userDocRef = doc(db, 'users', userAuth.uid);
+  console.log(userDocRef)
+
+  const userSnapshot = await getDoc(userDocRef);
+  console.log(userSnapshot)
+  console.log(userSnapshot.exists())
+
+}
