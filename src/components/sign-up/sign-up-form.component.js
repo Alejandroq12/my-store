61b52e2d6140c2576c0f1b707d190ae0478fb960
Alejandroq12 <julio.quezada@catolica.edu.sv>
2024-03-27 20:59:10 -0600

    Chore: Create default form values for the sign-up form.

--- src/components/sign-up/sign-up-form.component.js
+++ src/components/sign-up/sign-up-form.component.js
@@ -1,5 +1,12 @@
 import { useState } from 'react'
 
+const defaultFormFields = {
+  displayName: '',
+  email: '',
+  password: '',
+  confirmPassword: ' ',
+}
+
 const SignUpForm = () => {
   return (
     <div>
